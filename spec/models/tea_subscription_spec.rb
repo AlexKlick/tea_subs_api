9fc382c13b6af1637c563ac92ab6dd33c55663b4
Alex Klick <aklick12@gmail.com>
2021-11-04 22:24:51 -0600

    tea_sub random test setup

--- spec/models/tea_subscription_spec.rb
+++ spec/models/tea_subscription_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe TeaSubscription, type: :model do
   describe 'relationships' do
     it { should belong_to(:customer) }
-    it { should belong_to(:tea) }
   end
 end
